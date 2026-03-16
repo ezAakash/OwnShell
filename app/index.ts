cce43020349f50df0a7140df62eaaca4b56403cd
ezAakash <oenmazoro@gmail.com>
2026-03-16 23:50:43 +0530

    Add Locate executable files and echo

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,7 @@
 import { createInterface } from "node:readline";
+import Path from "path";
+import { access, constants, readdir } from "fs/promises"
+import type { PathLike } from "node:fs";
 
 const rl = createInterface({
   input: process.stdin,
@@ -7,10 +10,28 @@ const rl = createInterface({
 });
 
 const builtIns = ['echo', 'exit', 'type']
+const pathDir : string[] | undefined  = process.env.PATH?.split(Path.delimiter) || []
+
+
+async function searchForCommand(cmd: string): Promise<string | null> {
+    for (const path of pathDir!) {
+        const filePath: PathLike = Path.join(path ,cmd)
+        try{
+            await access(filePath, constants.X_OK)
+            return filePath
+        }
+        catch {
+            continue
+        }
+    }
+
+    return null
+}
+
 
 rl.prompt();
 
-rl.on('line', (command) => {//created a REPL here.
+rl.on('line', async (command) => {//created a REPL here.
   if ( command === 'exit') {
     rl.close()
     return 
@@ -25,16 +46,36 @@ rl.on('line', (command) => {//created a REPL here.
     }
 
     for (let i = 1; i < words.length; i++) {
+
+      if ( words[i] === "" ) {
+        continue
+      }
+
       if (builtIns.includes(words[i]!)){
         console.log(`${words[i]} is a shell builtin`)
-      }
-      else if (words[i] === "") {
         continue
+      }
+      
+      const filePath = await searchForCommand(words[i]!);
+    
+      if (!!filePath) {
+        console.log(`${words[i]} is ${filePath}`)
       }
       else {
         console.log(`${words[i]} not found`)
       }
     }
+  }
+  else if(command.startsWith('echo')) {
+    const words = command.split(/\s+/)
+
+    if ( words[0] !== 'echo') {
+      console.log(`${command}: command not found`)
+      return 
+    }
+
+    const output = words.slice(1).join(" ");
+    process.stdout.write(output + "\n");
   }
   else{
     console.log(`${command}: command not found`)
