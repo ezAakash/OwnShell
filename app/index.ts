9a5be457f38e8e7f79b316c39333114185a5ef7a
ezAakash <oenmazoro@gmail.com>
2026-03-16 16:18:01 +0530

    Add REPL loop and Add exit and echo commands

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,27 @@
-console.log("Building my own Shell!");
+import { createInterface } from "node:readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "$ ",
+});
+
+
+rl.prompt();
+
+rl.on('line', (command) => {//created a REPL here.
+  if ( command === 'exit') {
+    rl.close()
+    return 
+  }
+  else if( command.startsWith('echo ')) {
+    const startIndex = command.indexOf('echo') + 4
+    console.log(command.substring(5))
+    rl.prompt()
+  }
+  else{
+    console.log(`${command}: command not found`)
+  }
+
+  rl.prompt()
+})
